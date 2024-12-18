ee759587740a5e4dbf3a82a09d2bea01c3345aaf
jotasil88 <joaopauloluiz2005@gmail.com>
2024-12-18 09:29:56 -0300

    Creation of BoardException, implementation of defense programming in board class

--- src/boardLayer/Board.java
+++ src/boardLayer/Board.java
@@ -1,13 +1,16 @@
 package boardLayer;
 
 public class Board {
-	
+
 	private int rows;
 	private int columns;
-	
+
 	private Piece[][] pieces;
-	
+
 	public Board(int rows, int columns) {
+		if (rows < 1 && columns < 1) {
+			throw new BoardException("Erro ao criar tabuleiro: O tabuleiro deve ter ao menos 1 linha e 1 coluna! (" + rows + ", " + columns + ")");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -20,8 +23,13 @@ public class Board {
 	public int getColumns() {
 		return columns;
 	}
-	
+
 	public Piece piece(int row, int column) {
+		Position p = new Position(row, column);
+		if (!positionExists(p)) {
+			throw new BoardException("Erro ao retornar a peca: Essa posicao nao existe no tabuleiro! (" + p + ")");
+		}
+
 		return pieces[row][column];
 	}
 
@@ -30,6 +38,10 @@ public class Board {
 	}
 
 	public void placePiece(Piece piece, Position position) {
+		if (thereIsAPiece(position)) {
+			throw new BoardException("Erro ao colocar a peca: Ja existe uma peca nessa posição! (" + position + ")");
+		}
+
 		piece.position = position;
 		pieces[position.getRow()][position.getColumn()] = piece;
 	}
@@ -37,14 +49,22 @@ public class Board {
 //	public Piece removePiece(Position position) {
 //		
 //	}
-//
-//	public boolean positionExists(Position position) {
-//		
-//	}
-//
-//	public boolean thereIsAPiece(Position position) {
-//		
-//	}
+
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+
+	public boolean thereIsAPiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Erro ao verficar a peca: Essa posicao nao existe no tabuleiro! (" + position + ")");
+		}
+		
+		return piece(position) != null;
+	}
 
 	@Override
 	public String toString() {
--- src/chessLayer/ChessMatch.java
+++ src/chessLayer/ChessMatch.java
@@ -69,9 +69,9 @@ public class ChessMatch {
 //	}
 	
 	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.BLACK), new Position(0, 0));
+		board.placePiece(new Rook(board, Color.BLACK), new Position(0, 7));
 		board.placePiece(new King(board, Color.BLACK), new Position(0, 1));
-		board.placePiece(new Rook(board, Color.WHITE),new Position(0, 2));
+		board.placePiece(new Rook(board, Color.WHITE),new Position(0, 1));
 		
 	}
 	
